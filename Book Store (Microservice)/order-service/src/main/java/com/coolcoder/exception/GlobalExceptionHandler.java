83d8cec7e319bf6c4357fecd12c09b29eae2e98c
subrattech <beherasubrat5040@gmail.com>
2025-04-30 16:49:27 +0530

    Update GlobalExceptionHandler.java
    s

--- Book Store (Microservice)/order-service/src/main/java/com/coolcoder/exception/GlobalExceptionHandler.java
+++ Book Store (Microservice)/order-service/src/main/java/com/coolcoder/exception/GlobalExceptionHandler.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-/**
- * Handles exceptions across the whole order-service application.
- */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
