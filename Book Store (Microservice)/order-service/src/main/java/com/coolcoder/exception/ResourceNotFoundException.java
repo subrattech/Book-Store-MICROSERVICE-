6ee765bae9bad853522158f27523498064497983
subrattech <beherasubrat5040@gmail.com>
2025-04-30 16:49:39 +0530

    Update ResourceNotFoundException.java
    x

--- Book Store (Microservice)/order-service/src/main/java/com/coolcoder/exception/ResourceNotFoundException.java
+++ Book Store (Microservice)/order-service/src/main/java/com/coolcoder/exception/ResourceNotFoundException.java
@@ -1,8 +1,5 @@
 package com.coolcoder.exception;
 
-/**
- * Thrown when a requested resource (e.g., Order, User, Book) is not found.
- */
 public class ResourceNotFoundException extends RuntimeException {
 	public ResourceNotFoundException(String message) {
 		super(message);
